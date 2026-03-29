f7a223ebc31240a4d0f8b1ebadf38d1cfbb9aade
Vaibhav Pandey <codebyvaibhav@gmail.com>
2026-03-29 16:06:28 +0530

    added delete page

---
+++ delete/page.js
@@ -0,0 +1,11 @@
+export default function Delete() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Data Deletion Request</h1>
+      <p>
+        To request deletion of your data, please contact:
+        vaibhavvseo@gmail.com
+      </p>
+    </div>
+  );
+}
